e1ce9327d145daeda8fca75488b13829c20eb877
OUAZIN Nabil <nabilouazin91@gmail.com>
2022-12-22 03:37:00 +0100

    crispy forms

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import NewComment
 
-# Create your views here.
 posts = [
         {
           'title': 'التدوينة الاولى',
@@ -28,7 +29,36 @@ posts = [
 def home(request):
   
     context = {
-            'title': 'الثفحة الرئيسية',
-            'posts': posts,
+            'title': 'الصفحة الرئيسية',
+            'posts': Post.objects.all(),
             }
     return render(request, 'blog/index.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'من أنا'})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comments.filter(active=True)
+    comment_form = NewComment()
+    new_comment = None
+
+    context = {
+      'title': post,
+      'post': post,
+      'comments': comments,
+      'comment_form': comment_form,
+    }
+
+    if request.method == 'POST':
+        comment_form = NewComment(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = NewComment()
+    else:
+        comment_form = NewComment()
+
+    return render(request, 'blog/detail.html', context)
--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     # My apps
     'blog.apps.BlogConfig',
     'user.apps.UserConfig',
+    # Crispy Form
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
